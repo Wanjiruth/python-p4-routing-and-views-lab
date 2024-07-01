4832acbb84738126a0d69146f3fa9d4ba1834a7c
Wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-07-01 12:53:09 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = "\n".join(str(i) for i in range(parameter))
+    return f"{numbers}\n"
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return "Invalid input", 400
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation", 400
+    
+    # Convert result to int if it is an integer and not a division operation
+    if operation != 'div' and result.is_integer():
+        result = int(result)
+    
+    return str(result)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
